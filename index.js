fa55190ac9b79697d0879e6e539a30fa90b2869d
yuntingc <chiayunting@gmail.com>
2022-09-11 21:45:02 +0800

    base completed

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const { getSightings } = require('./utils.js')
-require('dotenv').config()
+const express = require("express");
+const { getSightings } = require("./utils.js");
+const cors = require("cors");
+require("dotenv").config();
 
+// console.log(process.env.PORT);
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(cors());
 
 app.get("/sightings", async (req, res) => {
   const sightings = await getSightings();
   res.json(sightings);
+});
+
+app.get("/sightings/:sightingIndex", async (req, res) => {
+  const sightings = await getSightings();
+  //console.log(req) --   params: { sightingIndex: '1' }
+  res.json(sightings[req.params.sightingIndex]);
 });
 
 app.listen(PORT, () => {
